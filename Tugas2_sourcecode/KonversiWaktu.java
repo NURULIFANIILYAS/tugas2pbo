95e47ea757b21a0ee9e66a4e910d64da63b418e1
NURULIFANIILYAS <102008979+NURULIFANIILYAS@users.noreply.github.com>
2022-03-22 17:45:05 +0800

    Add files via upload

---
+++ Tugas2_sourcecode/KonversiWaktu.java
@@ -0,0 +1,29 @@
+/* Nama                     : AULIA MAHARANI PUTRI IMALLINGKAAN */
+/* NIM                      : 13020200215 */
+/* HARI/ TANGGAL DAN WAKTU  : MINGGU/ 20 MARET 20:30 */
+/* KELAS                    : B2 */
+
+import java.util.Scanner;
+
+public class waktu {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        int totaljam, totalmenit, totaldetik,jamSekarang,  konversi;
+        
+        System.out.print("Berapa detik yang ingin anda konversikan : ");
+        konversi = input.nextInt();
+        
+        totaljam     = konversi/3600;
+        totalmenit   = (konversi%3600)/60;
+        totaldetik   = (konversi%3600)%60;
+        jamSekarang  = totaljam %24; 
+        
+        
+        System.out.println("Konversi dari : "+konversi+" detik, Adalah : ");
+        System.out.println(jamSekarang+" jam saat ini ");
+        System.out.println(totaljam+" jam, "+totalmenit+" menit, "+totaldetik+" detik");
+    }
+    
+    
+}
